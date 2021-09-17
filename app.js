bd51db7d0037e58549f18cb614705d7077d03ce9
juanm35 <jjmigone@gmail.com>
2021-09-17 00:08:19 -0300

    update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require ("express");
 const app = express();
 const path = require ("path");
-const PORT=process.env.PORT || 3000
+const puerto=process.env.PORT || 3000
 const publicPath = path.join(__dirname, "./public")
 
 
 app.use(express.static(publicPath))
 
-app. listen (PORT, function(){console.log("Se esta corriendo el Servidor en http://localhost:3000")});
+app. listen (puerto, function(){console.log("Se esta corriendo el Servidor en http://localhost:3000")});
 
 app.get("/", function(req,res){res.sendFile(path.join(__dirname,"./views/home.html"))})
 
