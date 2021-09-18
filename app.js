35e184b87aded550073782c887971851e60770c3
juanm35 <jjmigone@gmail.com>
2021-09-18 16:24:10 -0300

    form1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const app = express();
 const path = require ("path");
-const puerto=process.env.PORT || 3000
+const puerto= 3000
 const publicPath = path.join(__dirname, "./public")
 
 
@@ -10,6 +10,7 @@ app.use(express.static(publicPath))
 app. listen (puerto, function(){console.log("Se esta corriendo el Servidor en http://localhost:3000")});
 
 app.get("/", function(req,res){res.sendFile(path.join(__dirname,"./views/home.html"))})
+app.get("/form", function(req,res){res.sendFile(path.join(__dirname,"./views/form.html"))})
 
 
 
